9d16bfbf74a5fdefcfbd9cae8bbbc1a1d649f402
Ian Graham <i4ng1999@gmail.com>
2024-09-16 23:43:01 +0100

    Allow cross origin resource sharing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const { getEndpoints } = require('./controllers/api.controller.js');
 
@@ -24,6 +25,7 @@ const {
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api', getEndpoints);
 app.get('/api/users', getUsers);
