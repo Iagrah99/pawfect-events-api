a34b8901216ea7aee20754fcc6770625fcc55ac8
Ian Graham <i4ng1999@gmail.com>
2025-05-08 18:31:14 +0100

    Further update on categories for production db

--- db/data/development-data/categories.js
+++ db/data/development-data/categories.js
@@ -2,6 +2,7 @@ module.exports = [
   { slug: 'Dog-Show' },
   { slug: 'Dog-Walking' },
   { slug: 'Dog-Training' },
+  { slug: 'Dog-Competition' },
   { slug: 'Agility-Trials' },
   { slug: 'Herding-Trials' },
 ];
